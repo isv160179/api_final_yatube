97cde846fecb06678fc0ca6ba10360c268708d8d
Сергей Иванов <isv1601@yandex.ru>
2023-07-04 17:36:00 +0300

    PEP8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -6,8 +6,8 @@ MESSAGE = 'У Вас нет прав для изменения и удалени
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
